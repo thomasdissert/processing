66a5fef1ac63527c92ae526a054fdda06fdb714c
Thomas Dissert <thomas.dissert@student.kit.edu>
2021-02-08 18:07:23 +0100

    add code-position relation

--- triangles/sketch.js
+++ triangles/sketch.js
@@ -2,20 +2,20 @@ const TRIANGLE_WIDTH_HEIGHT = 80;
 var theta = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   background(255);
 }
 
 function draw() {
+  smooth();
   amountX = width / TRIANGLE_WIDTH_HEIGHT;
   amountY = height / TRIANGLE_WIDTH_HEIGHT;
 
   theta += 0.05;
 
-  var colorCode = map(sqrt(sin(theta) + 1), 0, 1.5, 64, 255);
-
   for (i = 0; i < amountX; i++) {
     for (j = 0; j < amountY; j++) {
+      var colorCode = map(sqrt(sin(theta - i * j) + 1), 0, 1.5, 64, 255);
       fill(colorCode);
       triangle(
         (i + 1) * TRIANGLE_WIDTH_HEIGHT,
